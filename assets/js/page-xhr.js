210a04002a4f52dbf6653cb4414731e8f4f799ae
Thomas <tom@axisj.com>
2021-05-10 14:17:15 +0900

    page-xhr

--- assets/js/page-xhr.js
+++ assets/js/page-xhr.js
@@ -12,6 +12,15 @@ fnObj.init = function () {
 fnObj.getUsers = function () {
   $.ajax({ url: "http://localhost:3000/users" }).done(function (data) {
     console.log(data);
+    $("#users-target").html(
+      "<ul>" +
+        data
+          .map(function (item) {
+            return "<li>Name : " + item.name + "</li>";
+          })
+          .join("") +
+        "</ul>"
+    );
   });
 };
 
